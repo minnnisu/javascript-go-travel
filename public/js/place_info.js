d9a299ea8866eed10d0d9467217dce8ecf091672
minsu_choi <mine52100@naver.com>
2022-09-07 22:50:16 +0900

    세부정보 페이지 블로그 추가

--- public/js/place_info.js
+++ public/js/place_info.js
@@ -1,8 +1,25 @@
+LatLng_y = $(".place-y").html();
+LatLng_x = $(".place-x").html();
+
 var mapContainer = document.getElementById("map"), // 지도를 표시할 div
   mapOption = {
-    center: new kakao.maps.LatLng($(".place-y").html(), $(".place-x").html()), // 지도의 중심좌표
-    level: 3, // 지도의 확대 레벨
+    center: new kakao.maps.LatLng(LatLng_y, LatLng_x), // 지도의 중심좌표
+    level: 1, // 지도의 확대 레벨
   };
 
 // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 var map = new kakao.maps.Map(mapContainer, mapOption);
+
+// 마커가 표시될 위치입니다
+var markerPosition = new kakao.maps.LatLng(LatLng_y, LatLng_x);
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+  position: markerPosition,
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+
+// 아래 코드는 지도 위의 마커를 제거하는 코드입니다
+// marker.setMap(null);
--- router/place.js
+++ router/place.js
@@ -1,15 +1,17 @@
 const axios = require("axios");
 const express = require("express");
 const router = express.Router();
+const headers = {
+  Authorization: "KakaoAK " + process.env.KAKAO_REST_API,
+};
 
 //location ID와 같은 장소 검색하여 반환하는 함수
 async function getInfoByLocation(placeID, query, y, x) {
   let placeInfo = null;
-  const headers = {
-    Authorization: "KakaoAK " + process.env.KAKAO_REST_API,
-  };
   for (let i = 1; i <= 3; i++) {
+    //결과를 찾은 경우 반환
     if (placeInfo != null) return placeInfo;
+
     const params = {
       query: query,
       page: i,
@@ -19,7 +21,6 @@ async function getInfoByLocation(placeID, query, y, x) {
       "https://dapi.kakao.com/v2/local/search/keyword.json?y=" + y + "&x=" + x,
       { params, headers }
     );
-
     result.data["documents"].forEach((element) => {
       if (element["id"] == placeID) {
         placeInfo = element;
@@ -28,16 +29,56 @@ async function getInfoByLocation(placeID, query, y, x) {
   }
 }
 
+//장소와 관련된 블로그 정보를 반환하는 함수
+async function getBlog(query) {
+  let blog_arr = []; //페이지별 결과를 담는 리스트
+
+  for (let page = 1; page <= 3; page++) {
+    const params = {
+      query: query,
+      page: page,
+    };
+
+    const result = await axios.get(
+      encodeURI("https://dapi.kakao.com/v2/search/blog.json?query=" + query),
+      { params, headers }
+    );
+
+    blog_arr = blog_arr.concat(result.data["documents"]); //기존의 검색결과와 합치기
+  }
+
+  for (let idx = 0; idx < blog_arr.length; idx++) {
+    const reg = /<[^>]*>?/g;
+    blog_arr[idx]["blogname"] = blog_arr[idx]["blogname"].replace(reg, "");
+    blog_arr[idx]["blogname"] = blog_arr[idx]["blogname"].replace(reg, "");
+    blog_arr[idx]["title"] = blog_arr[idx]["title"].replace(reg, "");
+    blog_arr[idx]["title"] = blog_arr[idx]["title"].replace(reg, "");
+    blog_arr[idx]["contents"] = blog_arr[idx]["contents"].replace(reg, "");
+    blog_arr[idx]["contents"] = blog_arr[idx]["contents"].replace(reg, "");
+  }
+  return blog_arr;
+}
+
 //장소 세부정보 페이지
 router.get("/place/info", async (req, res, next) => {
-  const data = await getInfoByLocation(
-    req.query.id,
-    req.cookies["placeQuery"],
-    req.cookies["DestinationY"],
-    req.cookies["DestinationX"]
-  );
-  res.render("place_info", {
-    place: data,
-  });
+  try {
+    const location_info = await getInfoByLocation(
+      req.query.id,
+      req.cookies["placeQuery"],
+      req.cookies["DestinationY"],
+      req.cookies["DestinationX"]
+    );
+
+    const blog = await getBlog(
+      req.cookies["DestinationName"] + " " + location_info["place_name"]
+    );
+    res.render("place_info", {
+      place: location_info,
+      blog_list: blog,
+    });
+  } catch (err) {
+    next(err);
+  }
 });
+
 module.exports = router;
