ace13484fc6f4592776c934f52edd4c57eeb46c5
minnnisu <mine52100@naver.com>
2022-10-31 19:37:00 +0900

    리스트 추가 모달 삭제 후 장소 자세히 보기 페이지 구현(이전 커밋이 잘못됨)

--- routes/place.js
+++ routes/place.js
@@ -2,29 +2,6 @@ const express = require("express");
 const router = express.Router();
 const api = require("../module/api");
 
-//장소 세부정보 페이지
-// router.get("/", async (req, res, next) => {
-//   try {
-//     const location_info = await api.getOneInfoByLocation(
-//       //여행지이름, 카테고리, 전화번호, 주소등의 정보를 받아옴
-//       req.query.id,
-//       req.query.name,
-//       req.query.y,
-//       req.query.x
-//     );
-
-//     const blog = await api.getBlog(req.query.name, req.query.y, req.query.x); //여행지와 관련된 블로그 정보를 가져옴
-
-//     res.render("place_info", {
-//       place: location_info,
-//       blog_list: blog,
-//     });
-//   } catch (err) {
-//     // next(err);
-//     next(new Error(err.message));
-//   }
-// });
-
 //키워드를 통한 특정 장소 검색
 router.get("/", async (req, res, next) => {
   let nick = null;
@@ -83,18 +60,25 @@ router.get("/destination", async (req, res, next) => {
   res.redirect("/");
 });
 
-router.get("/info", async (req, res, next) => {
+router.get("/info/add", async (req, res, next) => {
   try {
     const location_info = await api.getOneInfoByLocation(
-      //요청받은 하나의 장소에 대한 정보를 가져옴
-      req.query.id,
-      req.query.name,
+      //여행지이름, 카테고리, 전화번호, 주소등의 정보를 받아옴
+      req.query.placeId,
+      req.query.query,
       req.query.y,
       req.query.x
     );
-    res.send(location_info);
-  } catch (error) {
-    next(error);
+
+    const blog = await api.getBlog(req.query.query, req.query.y, req.query.x); //여행지와 관련된 블로그 정보를 가져옴
+
+    res.render("place_info_add", {
+      place: location_info,
+      blog: blog,
+    });
+  } catch (err) {
+    // next(err);
+    next(new Error(err.message));
   }
 });
 
